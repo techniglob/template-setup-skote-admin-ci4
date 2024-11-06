4f67a9032880da6ff57fa61da6d4d4acbbcb0698
Pritam Khan <pritamkhanofficial@gmail.com>
2024-11-06 20:55:17 +0530

    update

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,6 +92,3 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
-
-
-define('UPLOAD_DIR', WRITEPATH . 'uploads/');
--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -21,158 +21,38 @@ use Psr\Log\LoggerInterface;
  */
 abstract class BaseController extends Controller
 {
-  /**
-   * Instance of the main Request object.
-   *
-   * @var CLIRequest|IncomingRequest
-   */
-  protected $request;
+    /**
+     * Instance of the main Request object.
+     *
+     * @var CLIRequest|IncomingRequest
+     */
+    protected $request;
 
-  /**
-   * An array of helpers to be loaded automatically upon
-   * class instantiation. These helpers will be available
-   * to all other controllers that extend BaseController.
-   *
-   * @var array
-   */
-  protected $helpers = [];
+    /**
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
+     *
+     * @var list<string>
+     */
+    protected $helpers = [];
 
-  /**
-   * Be sure to declare properties for any property fetch you initialized.
-   * The creation of dynamic property is deprecated in PHP 8.2.
-   */
-  // protected $session;
+    /**
+     * Be sure to declare properties for any property fetch you initialized.
+     * The creation of dynamic property is deprecated in PHP 8.2.
+     */
+    // protected $session;
 
-  /**
-   * @return void
-   */
-  public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
-  {
-    // Do Not Edit This Line
-    parent::initController($request, $response, $logger);
+    /**
+     * @return void
+     */
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
 
-    // Preload any models, libraries, etc, here.
+        // Preload any models, libraries, etc, here.
 
-    // E.g.: $this->session = \Config\Services::session();
-
-    // return $this->response->setJSON($response);
-  }
-
-  public function fileHandle($crud = NULL, $field = NULL, $file_type = NULL)
-  {
-    if (!is_null($crud)) {
-      switch ($file_type) {
-        case 'image':
-          $accept = ".jpg, .jpeg, .png";
-          break;
-        case 'document':
-          $accept = ".pdf, .doc, .docx";
-          break;
-        case 'video':
-          $accept = ".mp4";
-          break;
-
-        default:
-          $accept = NULL;
-          break;
-      }
-      $crud->callbackColumn($field, array($this, 'showFile'));
-      $crud->callbackAddField(
-        $field,
-        function () use ($accept, $field) {
-          return  '<input id="field-' . $field . '" type="file" class="form-control  " accept="' . $accept . '" name="' . $field . '" value="">';
-        }
-      );
-
-      $crud->callbackEditField(
-        $field,
-        function ($data)  use ($accept, $field) {
-          $path = base_url() . 'uploads/' . $data;
-
-          $html = $this->showFile($data);
-          $html .= '<input id="field-image" type="file" class="form-control mt-2" accept="' . $accept . '" name="' . $field . '" value="">';
-
-          $html .= '<input id="field-image" type="hidden" class="form-control" name="file_hidden" value="' . $data . '">';
-          return $html;
-        }
-      );
-
-      $crud->callbackBeforeInsert(
-        function ($cbData) use ($field) {
-          $toUpload = $this->request->getFile($field);
-          if (isset($toUpload)) {
-            $image = UploadFile($toUpload);
-            $cbData->data[$field] = $image;
-
-            return $cbData;
-          }
-        }
-      );
-
-      $crud->callbackBeforeUpdate(
-        function ($cbData)  use ($field) {
-          $toUpload = $this->request->getFile($field);
-
-          $file_hidden = $this->request->getVar('file_hidden');
-
-          if (isset($toUpload)) {
-            $image = UploadFile($toUpload, null, $file_hidden);
-            $cbData->data[$field] = $image;
-          } else {
-            $cbData->data[$field] = $file_hidden;
-          }
-          $cbData->data['updated_by'] = getBUD()->id;
-          $cbData->data['updated_at'] = \getDbDate();
-          return $cbData;
-        }
-      );
-      $crud->fieldType('created_by', 'hidden', getBUD()->id);
-      $crud->fieldType('updated_at', 'hidden', null);
-      $crud->fieldType('updated_by', 'hidden', null);
-      return $crud;
+        // E.g.: $this->session = \Config\Services::session();
     }
-
-    return $crud;
-  }
-
-  public function showFile($value)
-  {
-    $path = "No File";
-    if (!empty($value)) {
-      $url = base_url('uploads/');
-      $type = pathinfo($value)['extension'];
-      // die($type);
-      $icon = '';
-      if ($type === 'pdf') {
-        $icon = '<i class=" fas fa-file-pdf text-danger  fs-1  "></i>';
-      } elseif ($type === 'xls' || $type === 'xlsx') {
-        $icon = '<i class="fas fa-file-excel text-success  fs-1  "></i>';
-      } elseif ($type === 'doc' || $type === 'docx') {
-        $icon = '<i class=" fas fa-file-word  text-primary   fs-1  "></i>';
-      } elseif ($type === 'txt') {
-        $icon = '<i class="fas fa-images fs-1"></i>';
-      } else {
-        $icon = '<i class="fas fa-images"></i>';
-      }
-
-      // $icon = $type;
-      if ($type == 'pdf' or $type == 'xls' or $type == 'xlsx' or $type == 'doc' or $type == 'docx' or $type == 'txt') {
-
-        $path = '<a target="_new" href="' . $url . $value . '"> ' . $icon . ' </a>';
-      } elseif ($type == 'png' or $type == 'jpg' or $type == 'jpeg' or $type == 'bmp' or $type == 'webp' or $type == 'gif') {
-        $path = '<img src=' . $url . $value . ' height="100" width="100">';
-      }
-    }
-
-
-    return $path;
-  }
-
-  public function respond($data = [], $statusCode = 200){
-    $response = service('response');
-    if(is_string($statusCode)){
-      $statusCode = (int)$statusCode;
-    }
-    return $response->setStatusCode($statusCode)->setJSON($data);
-  }
 }
