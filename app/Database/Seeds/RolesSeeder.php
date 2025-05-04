fe5388c9e50704196c40d97c877debcad8b2d4c5
Pritam Khan <pritamkhanofficial@gmail.com>
2025-05-04 12:21:19 +0530

    rele seeder changes

--- app/Database/Seeds/RolesSeeder.php
+++ app/Database/Seeds/RolesSeeder.php
@@ -10,13 +10,13 @@ class RolesSeeder extends Seeder
     {
         $data = [
             [
-                'role_name'       => 'admin',
-                'display_name'    => 'Admin',
+                'role_name'       => 'super_admin',
+                'display_name'    => 'Super admin',
                 'created_at'      => date('Y-m-d H:i:s'),
                 'created_by'      => 1
             ], [
-                'role_name'       => 'customer',
-                'display_name'    => 'Customer',
+                'role_name'       => 'admin',
+                'display_name'    => 'Admin',
                 'created_at'      => date('Y-m-d H:i:s'),
                 'created_by'      => 1
             ]
