97439f820d5378b58417de92819ed0b7edf27d7b
Pritam Khan <pritamkhanofficial@gmail.com>
2025-05-11 00:25:43 +0530

    update

--- app/Views/Components/Portal/head.php
+++ app/Views/Components/Portal/head.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-<title>Dashboard | Skote - Admin & Dashboard Template</title>
+<title>Welcome Back – Please Log In</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
 <meta content="Themesbrand" name="author" />
